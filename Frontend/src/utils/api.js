b70fc7d10d198f8f77a303424b22f2b7d51be5fa
pawan kumar <meroy0075@gmail.com>
2026-01-17 15:04:40 +0530

    Update api.js

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "https://reguim-innovations.onrender.com"
+    baseURL: "https://reguim-innovations.onrender.com",
     withCredentials: true
 })
 
