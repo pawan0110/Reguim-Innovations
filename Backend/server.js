6e44ff1ac5998b3029fadaf6fa9b321f4ec44337
pawan kumar <meroy0075@gmail.com>
2026-01-17 15:03:14 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,8 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173", 
-  "http://localhost:5174", 
+  "http://localhost:5174",
+  "https://reguim-innovations.vercel.app/"
    
 ];
 
